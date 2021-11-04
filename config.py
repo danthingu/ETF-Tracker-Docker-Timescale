f01465b00a5afd14dbd3044f5d9658026222ef27
thien <thienng93@gmail.com>
2021-11-03 23:24:16 -0700

    add prices script for each stock

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ API_URL='https://paper-api.alpaca.markets'
 API_KEY='PKEBYVRHR849A8Y96PWV'
 API_SECRET='nG8zbWYZwv9ycV0NsNiUOrnzBIxDzk8WKKpH7vLp'
 
+POLYGON_API_KEY='Xe0TCOlitNLtY5nsf1rA_5xFVWgpY3tX'
+
 DB_HOST='localhost'
 DB_USER='postgres'
 DB_PASS='password'
